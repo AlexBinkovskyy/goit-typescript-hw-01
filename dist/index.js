0a4dbb832adf2734d722409b025d748acaf72dbf
AlexBinkovskyy <alex_bin@ukr.net>
2024-03-13 15:37:10 +0200

    extension added

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,12 @@
-import { concatenation } from './concatenation';
-const button = document.querySelector('button');
-const input = document.querySelector('input');
+import { concatenation } from "./concatenation.js";
+const button = document.querySelector("button");
+const input = document.querySelector("input");
 if (button && input) {
-    button.addEventListener('click', () => {
-        concatenation(input.value, 'hello!');
+    button.addEventListener("click", () => {
+        concatenation(input.value, "hello!");
     });
 }
+function handleEvent(event, handler) { }
+handleEvent("click", () => { console.log("Clicked!"); });
+handleEvent("mouseover", (x, y) => { console.log(`Mouse at ${x}, ${y}`); });
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
-import { concatenation } from './concatenation';
+import { concatenation } from "./concatenation";
 
-const button = document.querySelector('button')!;
-const input = document.querySelector('input')!;
+const button = document.querySelector("button")!;
+const input = document.querySelector("input")!;
 
 if (button && input) {
-  button.addEventListener('click', () => {
-    concatenation(input.value, 'hello!');
-  });
+    button.addEventListener("click", () => {
+        concatenation(input.value, "hello!");
+    });
 }
+function handleEvent(event: string, handler: (params: any[]) => void): void {  }
+handleEvent("click", () => { console.log("Clicked!") }); handleEvent("mouseover", (x: number, y: number) => { console.log(`Mouse at ${x}, ${y}`); });
